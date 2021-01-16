1c3a8001f4116f9542612391c781ce9ee4b6e78a
timwibiral <tim.wibiral@uni-ulm.de>
2021-01-16 22:34:11 +0100

    Reverting to old implementation and added documentation

--- src/Model/CustomDataset.py
+++ src/Model/CustomDataset.py
@@ -1,19 +1,19 @@
 import torch
-import Model
-from functools import lru_cache
 
 
 class CustomDataset(torch.utils.data.Dataset):
-    """ Special dataset that is used for ChessANNv2 and delivers two input tensors in stead of one. """
+    """
+    Custom dataset that delivers two input tensors instead of one.
+    Takes one tensor for all conv inputs, the player on turn and the results
+    """
 
-    def __init__(self, positions_list: list):
-        self.positions = positions_list
+    def __init__(self, conv_input, player_input, result):
+        self.conv_input = conv_input
+        self.player_input = player_input
+        self.result = result
 
     def __len__(self):
-        return len(self.positions)
+        return len(self.result)
 
-    @lru_cache(1000000)
     def __getitem__(self, idx):
-        sample = self.positions[idx]
-        # position, on turn, label
-        return Model.process_epd(sample[0]), sample[1], sample[2]
+        return self.conv_input[idx], self.player_input[idx], self.result[idx]
